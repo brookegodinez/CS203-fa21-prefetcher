6f6587c40c7763147d02e913b71f8737d8cfcfa9
brookegodinez <brookegodinez@hotmail.com>
2021-11-24 15:06:47 -0800

    Most recent prefetcher idea

--- prefetcher.C
+++ prefetcher.C
@@ -10,17 +10,117 @@
 
 #include "prefetcher.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unordered_map>
 
-Prefetcher::Prefetcher() { _ready = false; }
-
-bool Prefetcher::hasRequest(u_int32_t cycle) { return _ready; }
-
-Request Prefetcher::getRequest(u_int32_t cycle) { return _nextReq; }
-
-void Prefetcher::completeRequest(u_int32_t cycle) { _ready = false; }
-
-void Prefetcher::cpuRequest(Request req) { 
-	if(!_ready && !req.HitL1) {
-		_ready = false;
-	}
+Prefetcher::Prefetcher() { _ready = false; stride = 16;
+	last_reference.addr = 0;
+	global_stride = 0;  
+	global_state = 0; 
 }
+
+bool Prefetcher::hasRequest(u_int32_t cycle) {  
+	if (!_ready){
+		if (stream_buffer[cycle%5] != 0){
+			_nextReq.addr = stream_buffer[cycle%5];
+			stream_buffer[cycle%5] = 0;
+			_ready = true;
+			}
+	}
+	
+	return _ready;} 
+
+Request Prefetcher::getRequest(u_int32_t cycle) {
+	return _nextReq; }
+
+void Prefetcher::completeRequest(u_int32_t cycle) {
+
+	_ready = false; }
+
+
+
+void Prefetcher::cpuRequest(Request req) {  
+	bool found_entry = false;
+	bool global_bool = false; 
+	global_stride = last_reference.addr - req.addr; 
+
+	if(!_ready && !req.HitL1) {
+		if (req.addr == last_reference.addr + global_stride){
+			printf("global stride correct %d", global_stride);
+			if (global_state<4){
+			global_state++;
+			// global_stride = last_reference.addr - req.addr; 
+
+			}
+		}
+		else {
+			// printf("global stride wrong %d  ", global_stride);
+			if(global_state>0){
+				global_state--;
+				// global_stride = req.addr - last_reference.addr; 
+			}
+		}
+		
+		for (int i = 0; i < pre_array.size(); i++){
+		if(pre_array[i][0] == last_reference.addr){
+			if (req.addr != last_reference.addr + pre_array[i][2]){  //check if current req is equal to the sride + last request
+				pre_array[i][2] = req.addr - pre_array[i][1]; // if not update the stride
+				if (pre_array[i][3] > 0){ //checks state
+					pre_array[i][3]--;
+				}
+			}
+			else{
+				if (pre_array[i][3] < 4){
+					pre_array[i][3]++;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < pre_array.size(); i++){
+		if (pre_array[i][0] == req.pc){
+			pre_array[i][1] = last_reference.addr;
+			found_entry = true;
+			if(pre_array[i][3] >= 2){
+				_nextReq.addr = req.addr + pre_array[i][2];
+				_ready = true; 
+				// printf("take stride");
+				for (int j = 0; j < 5; j++){
+					stream_buffer[j] = req.addr + pre_array[i][2] * (j+1);
+					}
+			}
+		}
+	}
+				if (global_state >= 2){
+				_nextReq.addr = req.addr + global_stride;
+				_ready = true; 
+				for (int j = 0; j < 5; j++){
+					stream_buffer[j] = req.addr + (global_stride * (j+1));
+					}
+				}
+				else {
+					_nextReq.addr = req.addr + stride;
+					_ready = true; 
+				for (int j = 0; j < 5; j++){
+					stream_buffer[j] = req.addr + (stride * (j+1));
+					}
+				}
+	if (!found_entry){
+		std::vector<u_int32_t>temp_array;
+		temp_array.push_back(req.pc);
+		temp_array.push_back(last_reference.addr);
+		temp_array.push_back(stride);
+		temp_array.push_back(1);
+		pre_array.push_back(temp_array);
+		_nextReq.addr = req.addr + stride; 
+		_ready = true; 
+		// last_reference.addr = req.addr;
+	}
+	// global_stride = last_reference.addr - req.addr; 
+	// last_reference.addr = req.addr; 
+	}
+	last_reference.addr = req.addr; 
+
+	// global_stride = last_reference.addr - req.addr; 
+
+	}
+	
--- prefetcher.h
+++ prefetcher.h
@@ -11,29 +11,51 @@
 
 #include <sys/types.h>
 #include "mem-sim.h"
+#include <vector>
+#include <unordered_map>
 
 class Prefetcher {
   private:
 	bool _ready;
 	Request _nextReq;
+	u_int32_t last_miss;
+	u_int32_t last_hit;
+	u_int32_t stride;
+	u_int32_t stream_buffer[10];
+	std::vector<u_int32_t> stride_calc;
+	u_int32_t l2_stream_array[20];
+	std::vector<Request> holding_references;
+	int count;
+	std::unordered_map<u_int32_t, u_int32_t> map; 
+	Request last_reference;
+	bool hit_in_map;
+	// int count; 
+	u_int32_t dist_array[200];
+	std::vector<std::vector<u_int32_t> > pre_array;
+	u_int32_t global_stride;  
+	int global_state; 
+	
+	
 
   public:
 	Prefetcher();
-
+	int count_nums(){ return count; }
 	// should return true if a request is ready for this cycle
 	bool hasRequest(u_int32_t cycle);
 
 	// request a desired address be brought in
 	Request getRequest(u_int32_t cycle);
-
+	int print_stride(void); 
 	// this function is called whenever the last prefetcher request was successfully sent to the L2
 	void completeRequest(u_int32_t cycle);
-
+	int stride_check(void);
+	void print_references(void);
 	/*
 	 * This function is called whenever the CPU references memory.
 	 * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
 	 */
 	void cpuRequest(Request req); 
+	// void stateMachine(int state);
 };
 
 #endif
