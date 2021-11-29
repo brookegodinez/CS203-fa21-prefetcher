9c69065df332a70ec7ee75e4a71b1ae1342cab56
brookegodinez <brookegodinez@hotmail.com>
2021-11-28 19:05:11 -0800

    finished my prefetcher

--- prefetcher.C
+++ prefetcher.C
@@ -17,17 +17,18 @@ Prefetcher::Prefetcher() { _ready = false; stride = 16;
 	last_reference.addr = 0;
 	global_stride = 0;  
 	global_state = 0; 
+	for (int i = 0; i < 16; i++){
+		global_table.push_back(1);
+		global_stride_table.push_back(stride);
+	}
 }
 
 bool Prefetcher::hasRequest(u_int32_t cycle) {  
 	if (!_ready){
-		if (stream_buffer[cycle%5] != 0){
 			_nextReq.addr = stream_buffer[cycle%5];
-			stream_buffer[cycle%5] = 0;
 			_ready = true;
 			}
-	}
-	
+
 	return _ready;} 
 
 Request Prefetcher::getRequest(u_int32_t cycle) {
@@ -41,34 +42,33 @@ void Prefetcher::completeRequest(u_int32_t cycle) {
 
 void Prefetcher::cpuRequest(Request req) {  
 	bool found_entry = false;
-	bool global_bool = false; 
-	global_stride = last_reference.addr - req.addr; 
-
-	if(!_ready && !req.HitL1) {
-		if (req.addr == last_reference.addr + global_stride){
-			printf("global stride correct %d", global_stride);
-			if (global_state<4){
+	if (req.addr == last_reference.addr + global_stride_table[global_state]){
+			if (global_table[global_state] < 4){ //check if global state needs t be updated
+				global_table[global_state]++;
+			} //decrement and increment accordingly 
+			if (global_state<15){
 			global_state++;
-			// global_stride = last_reference.addr - req.addr; 
-
 			}
+			
 		}
-		else {
-			// printf("global stride wrong %d  ", global_stride);
+		else {//if not correct the global state is updated
+			global_stride_table[global_state] = req.addr - last_reference.addr;
+			if (global_table[global_state] > 0){ // 
+				global_table[global_state]--;
+			}
 			if(global_state>0){
 				global_state--;
-				// global_stride = req.addr - last_reference.addr; 
 			}
 		}
-		
+	if(!_ready && !req.HitL1) {		
 		for (int i = 0; i < pre_array.size(); i++){
 		if(pre_array[i][0] == last_reference.addr){
-			if (req.addr != last_reference.addr + pre_array[i][2]){  //check if current req is equal to the sride + last request
+			if (req.addr != last_reference.addr + pre_array[i][2]){   //check if current req is equal to the sride + last request
 				pre_array[i][2] = req.addr - pre_array[i][1]; // if not update the stride
 				if (pre_array[i][3] > 0){ //checks state
 					pre_array[i][3]--;
 				}
-			}
+			}//updates the local stride
 			else{
 				if (pre_array[i][3] < 4){
 					pre_array[i][3]++;
@@ -77,50 +77,45 @@ void Prefetcher::cpuRequest(Request req) {
 		}
 	}
 	for (int i = 0; i < pre_array.size(); i++){
-		if (pre_array[i][0] == req.pc){
+		if (pre_array[i][0] == req.addr){
 			pre_array[i][1] = last_reference.addr;
 			found_entry = true;
 			if(pre_array[i][3] >= 2){
-				_nextReq.addr = req.addr + pre_array[i][2];
+				_nextReq.addr = req.addr + pre_array[i][2];//actuall does the prefetching
 				_ready = true; 
-				// printf("take stride");
 				for (int j = 0; j < 5; j++){
-					stream_buffer[j] = req.addr + pre_array[i][2] * (j+1);
+					stream_buffer[j] = req.addr + pre_array[i][2] * (j+1);  //creating a stream buffer using the local stride
 					}
 			}
 		}
-	}
-				if (global_state >= 2){
-				_nextReq.addr = req.addr + global_stride;
+	}			//if not found in references global fetcher is used
+				if (global_table[global_state] >= 2 && !found_entry){
+				_nextReq.addr = req.addr + global_stride_table[global_state];//global_stride;
 				_ready = true; 
-				for (int j = 0; j < 5; j++){
-					stream_buffer[j] = req.addr + (global_stride * (j+1));
+					for (int j = 0; j < 5; j++){
+						stream_buffer[j] = req.addr + (global_stride_table[global_state] * j+1);//(global_stride * (j+1));
 					}
 				}
-				else {
+				else {//if global state is Not taken then a standard stride is used
 					_nextReq.addr = req.addr + stride;
 					_ready = true; 
-				for (int j = 0; j < 5; j++){
-					stream_buffer[j] = req.addr + (stride * (j+1));
+					for (int j = 0; j < 5; j++){
+						stream_buffer[j] = req.addr + (stride * (j+1)); //stream_buffer is populated
 					}
 				}
-	if (!found_entry){
+	if (!found_entry){ //push new reference to the vector if not found
+		if (pre_array.size() > 200){
+			pre_array.erase(pre_array.begin());
+		}
 		std::vector<u_int32_t>temp_array;
-		temp_array.push_back(req.pc);
+		temp_array.push_back(req.addr);
 		temp_array.push_back(last_reference.addr);
 		temp_array.push_back(stride);
-		temp_array.push_back(1);
+		temp_array.push_back(0);
 		pre_array.push_back(temp_array);
-		_nextReq.addr = req.addr + stride; 
-		_ready = true; 
-		// last_reference.addr = req.addr;
-	}
-	// global_stride = last_reference.addr - req.addr; 
-	// last_reference.addr = req.addr; 
-	}
-	last_reference.addr = req.addr; 
+		}
 
-	// global_stride = last_reference.addr - req.addr; 
-
+	}
+	last_reference.addr = req.addr; //last_reference is updated. 
 	}
 	
